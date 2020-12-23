a809b17f78aa281b4a71005fd0a44790e69b4da7
unknown <hasanarshi@gmail.com>
2020-12-24 00:00:53 +0530

    add two files in repo

---
+++ ArrayDemoP1.java
@@ -0,0 +1,22 @@
+public class ArrayDemoP1 
+{
+	public static void main(String[] args) 
+	{
+		int[] a;
+		a = new int[2];
+		
+		Integer[] ia = new Integer[2];
+		ia[0] = null;
+		
+		int b[];
+		b = new int[2];
+		
+		int[] i,j;
+		i = a;
+		j = a;
+		
+		int k[], l;
+		k = a;
+		l = a[0];
+	}
+}
